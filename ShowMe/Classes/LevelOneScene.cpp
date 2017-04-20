91477162aa988eeaa0a40234f2668e599c8d933b
dat <dat.nguyen.supper@gmail.com>
2017-04-20 07:19:45 +0700

    Show me : Update camera posion Test 9patch image

--- ShowMe/Classes/LevelOneScene.cpp
+++ ShowMe/Classes/LevelOneScene.cpp
@@ -1,5 +1,6 @@
 #include "LevelOneScene.h"
 
+
 USING_NS_CC;
 
 Scene* LevelOneScene::createScene()
@@ -29,23 +30,44 @@ bool LevelOneScene::init()
         return false;
     }
     
+    ninePath = cocos2d::ui::Scale9Sprite::create("CloseNormal.png", cocos2d::Rect::ZERO, Rect(10, 10, 20, 20));
+    ninePath->setScale9Enabled(true);
+    ninePath->setContentSize(Size(300,40));
+//    ninePath->setScaleX(5);
+    ninePath->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+    ninePath->setAnchorPoint(Vec2(0.5, 0.5));
+    addChild(ninePath);
+    
 	auto label = Label::createWithSystemFont("LevelOneScene", "Arial", 30);
 	label->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 	this->addChild(label);
 
 	createPlayer();
+    
+    
+    this->scheduleUpdate();
 
     return true;
 }
 
+
+
+void LevelOneScene::update(float delta){
+    
+    Vec2 pos = Vec2(player->getPositionX(), player->getPositionY());
+    auto cam = Camera::getDefaultCamera();
+//    cam->setPosition(pos);
+}
+
+
 void LevelOneScene::createPlayer() {
 
-	auto playerRed = Sprite::create("CloseNormal.png");
-	playerRed->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
-	playerRed->setAnchorPoint(Vec2(0.5, 0));
-	playerRed->setColor(Color3B::RED);
+	player = Sprite::create("CloseNormal.png");
+	player->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+	player->setAnchorPoint(Vec2(0.5, 0));
+	player->setColor(Color3B::RED);
 	//playerRed->setScaleX(2);
-	auto playerBodyRed = PhysicsBody::createBox(Size(playerRed->getContentSize().width, playerRed->getContentSize().height),
+	auto playerBodyRed = PhysicsBody::createBox(Size(player->getContentSize().width, player->getContentSize().height),
 		PhysicsMaterial(0.1f, 1.0f, 0.0f));
 	//playerBodyRed->setPositionOffset(Vec2(0, playerRed->getContentSize().height/4));
 	//set the body isn't affected by the physics world's gravitational force
@@ -53,10 +75,11 @@ void LevelOneScene::createPlayer() {
 	playerBodyRed->setDynamic(true);
 	playerBodyRed->setCollisionBitmask(PLAYER_COLLISION_BITMASK_RED);
 	playerBodyRed->setContactTestBitmask(true);
-	playerRed->setPhysicsBody(playerBodyRed);
+	player->setPhysicsBody(playerBodyRed);
 	//playerRed->addComponent(playerBodyRed);
-	addChild(playerRed);
-
+	addChild(player);
+    
+    
 }
 
 
--- ShowMe/Classes/LevelOneScene.h
+++ ShowMe/Classes/LevelOneScene.h
@@ -1,12 +1,17 @@
 #pragma once
 
 #include "BaseScene.h"
+#include "ui/CocosGUI.h"
 
 class LevelOneScene : public BaseScene{
+private:
+    cocos2d::Sprite * player;
+    cocos2d::ui::Scale9Sprite * ninePath;
 public:
 	static cocos2d::Scene* createScene();
 
     virtual bool init();
+    void update(float delta);
 	void createPlayer();
 
 	// implement the "static create()" method manually
